dbc5c5ed9c74933152be413ae0571d7077f6ac4f
dclord <daniellord.1988@googlemail.com>
2016-03-07 20:50:42 +0000

    updated logcheck2 script to remove unnecessesary file opens

--- scripts/logcheck2.py
+++ scripts/logcheck2.py
@@ -7,7 +7,6 @@ import sys
 try:
     #open file
     file = open('/var/log/auth.log')
-    write = open('/home/dan/bannedip')
     # create empty list
     ips = []
     # read text through file
@@ -25,11 +24,11 @@ try:
 	add = "".join(ip)
 	if add is not '':
 #leaving in as learning - Not needed- text = add.strip('[')
-		with open('/home/dan/bannedip', 'a') as bannedip:
+		with open('/home/root/bannedip', 'a') as bannedip:
 			bannedip.write(add + "\n")
+		bannedip.close()
     # close file
     file.close()
-    write.close()
 
 except IOError as err:
 	print('oops error: %s') % err
