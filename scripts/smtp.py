f84f32e50d70abbfbcf78ddf5a9438a4ead560a1
dclord <daniellord.1988@googlemail.com>
2016-03-08 20:07:33 +0000

    SMTP Script Added

---
+++ scripts/smtp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+msg = MIMEMultipart()
+msg['From'] = 'Email address'
+msg['To'] = 'to email address'
+msg['Subject'] = 'Subject header'
+message = 'here is the body of the email'
+msg.attach(MIMEText(message))
+
+mailserver = smtplib.SMTP('mail server address', port)
+#identify to mail server
+mailserver.ehlo()
+#start TLS
+mailserver.starttls()
+#re-id after encrpytion
+mailserver.ehlo()
+mailserver.login('user', 'pass')
+
+mailserver.sendmail('From', 'To',msg.as_string())
+
+mailserver.quit()
+
