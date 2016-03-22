b949f37a9c210f6e550606abbdff8e75409309e9
dclord <daniellord.1988@googlemail.com>
2016-03-22 20:57:26 +0000

    added googlesearch

---
+++ scripts/webscraping/googlesearch.py
@@ -0,0 +1,26 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
+#create firefox window
+driver = webdriver.Firefox()
+
+#navgate to google.co.uk
+driver.get("http://www.google.co.uk")
+
+#find element by ID - lst-ib = google search box
+elem = driver.find_element_by_id("lst-ib")
+
+#type python in google search
+elem.send_keys("python")
+
+#press enter
+elem.send_keys(Keys.RETURN)
+assert "No Results Found" not in driver.page_source
+
+#wait 600 seconds
+driver.implicitly_wait(600)
+
+#close browser 
+driver.close()
