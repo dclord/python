52a0bc38be0d8fe17dfb41a128789664da29146d
dclord <daniellord.1988@googlemail.com>
2016-06-21 20:46:23 +0100

    Update fabfile.py

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -6,12 +6,29 @@ from fabric.api import *
 env.user = 'user'
 env.hosts = ['mail01', 'web01', 'ns01', 'zabbix']
 
+
+##################################################
+##### Runs 'sudo apt-get update' on each     #####
+##### env.hosts listed.                      #####
+##################################################
+
 @parallel
 def aptupd():
 	sudo('apt-get update')
 	
+##################################################
+##### Copies apticron config from current    #####
+##### server to all env.hosts                #####
+##################################################
+
 def copy_conf():
 	put("/etc/apticron/apticron.conf", "/etc/apticron/", use_sudo=True)
+
+
+##################################################
+##### Runs uptime command then writes the    #####
+##### output to the logfile.		     #####
+##################################################
 
 @parallel
 def uptime():
@@ -22,6 +39,11 @@ def uptime():
 
 	f.close
 
+
+##################################################
+##### Shuts down the entire env.hosts	     #####
+##### Beware using this function 	     #####
+##################################################
 	
 def shutdown():
 		sudo("shutdown -h now")
