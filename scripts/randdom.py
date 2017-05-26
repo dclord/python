24f640b3f668a022c6ae5a49957551d864ee8275
dclord <daniellord.1988@googlemail.com>
2017-05-26 05:58:53 +0100

    Update randdom.py

--- scripts/randdom.py
+++ scripts/randdom.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python
 import random
+import urllib
 
 def generate():
     d = ['.com', '.org', '.co.uk', '.org.uk']
     u = ''.join([random.choice('abcdefghijklmnopqrstuvwyxz') for x in range(random.randint(30, 40))])
     r = random.choice(d)
-    print u + r
+    l = u + r
+    get(l);
+
+def get(l):
+    try:
+        urllib.urlopen(l)
+        print "Connected" + l
+    except Exception, e:
+        print "Unable to connect - " + l
 
 if __name__ == "__main__":
     x = 0
